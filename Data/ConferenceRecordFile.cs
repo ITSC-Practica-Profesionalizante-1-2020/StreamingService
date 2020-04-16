a7188569bcaac2bc9fba8c1198c7cf6b31aabb43
Ezequiel Monforte <ezequielmonforte@gmail.com>
2020-04-16 19:45:41 -0300

    Initial commit

---
+++ Data/ConferenceRecordFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+	class ConferenceRecordFile: EntityBase
+	{
+		public IUser Owner { get; set; }
+		public string Path { get; set; }
+	}
+}
