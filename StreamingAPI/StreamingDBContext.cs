71f88a4b7c626deb01d96de2dd33634a32785c6f
Ezequiel Monforte <monforte.ezequiel@gpcloud.com>
2020-08-17 17:13:55 -0300

    Database initial migration

---
+++ StreamingAPI/StreamingDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedModels.StreamingService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamingAPI
+{
+	public class StreamingDBContext : DbContext
+	{
+		public StreamingDBContext(DbContextOptions<StreamingDBContext> options)
+			: base(options)
+		{ }
+
+		public DbSet<VideoRoom> VideoRooms { get; set; }
+		public DbSet<ConferenceRecordFile> ConferenceRecordFiles { get; set; }
+
+	}
+}
